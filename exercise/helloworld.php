5623d9b68fce56df03db9dd6e6b4ffe19b287943
Bùi Mạnh Tuấn <51566591+bmtuan@users.noreply.github.com>
2021-03-25 21:47:37 +0700

    commit exercise

---
+++ exercise/helloworld.php
@@ -0,0 +1,19 @@
+<?php
+$post = $_POST;
+$name = $post['name'];
+$birthday = $post['birthday'];
+$phone = $post['phone'];
+$email = $post['email'];
+$class = $post['class'];
+$university = $post['university'];
+$hobby = $post['hobby'];
+echo('Tên của bạn là: '.$name.'<br>');
+echo('Ngày sinh của bạn là: '.$birthday.'<br>');
+echo('Email của bạn là: '.$email.'<br>');
+echo('Số điện thoại của bạn là: '.$phone.'<br>');
+echo('Lớp của bạn là: '.$class.'<br>');
+echo('Trường của bạn là: '.$university.'<br>');
+foreach($hobby as $key => $item){
+    $n = $key+1;
+    echo($n.' '.$item.'<br>');
+};
