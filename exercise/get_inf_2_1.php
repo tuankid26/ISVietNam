276d5076c5e488a8140b227dba9f2765e1f6694d
Bùi Mạnh Tuấn <51566591+bmtuan@users.noreply.github.com>
2021-03-31 23:11:22 +0700

    exercise

--- exercise/get_inf_2_1.php
+++ exercise/get_inf_2_1.php
@@ -6,15 +6,19 @@ $phone = $post['phone'];
 $email = $post['email'];
 $class = $post['class'];
 $university = $post['university'];
-$hobby = $post['hobby'];
-echo('Tên của bạn là: '.$name.'<br>');
-echo('Ngày sinh của bạn là: '.$birthday.'<br>');
-echo('Email của bạn là: '.$email.'<br>');
-echo('Số điện thoại của bạn là: '.$phone.'<br>');
-echo('Lớp của bạn là: '.$class.'<br>');
-echo('Trường của bạn là: '.$university.'<br>');
-echo('Sở thích của bạn là:<br>');
-foreach($hobby as $key => $item){
-    $n = $key+1;
-    echo($n.'. '.$item.'<br>');
-};
+echo('Tên của bạn là: <b>'.$name.'</b><br><br>');
+echo('Ngày sinh của bạn là: <b>'.$birthday.'</b><br><br>');
+echo('Email của bạn là: <b>'.$email.'</b><br><br>');
+echo('Số điện thoại của bạn là: <b>'.$phone.'</b><br><br>');
+echo('Lớp của bạn là: <b>'.$class.'</b><br><br>');
+echo('Trường của bạn là: <b>'.$university.'</b><br><br>');
+if (isset($post['hobby'])){
+    echo('Sở thích của bạn là:<br>');
+    foreach($post['hobby'] as $key => $item){
+        $n = $key+1;
+        echo($n.'. <i>'.$item.'</i><br>');
+    }
+}
+else{
+    echo('<i>Bạn không có sở thích nào!!! :(<i>');
+}
--- exercise/get_inf_2_2.php
+++ exercise/get_inf_2_2.php
@@ -1,98 +1,97 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="./assets/style2_2.css">
+    <link rel="stylesheet" href="./assets/style_2_2.css">
 </head>
 
 <body>
+<?php
+    $post = $_POST;
+    $name = $post['name'];
+    $second = $post['second'];
+    $minute = $post['minute'];
+    $hour = $post['hour'];
+    $date = $post['date'];
+    $month = date('m', strtotime($date));
+    $year = date('y', strtotime($date));
+    $day = date('d', strtotime($date));
+    $m31 = array(1, 3, 5, 7, 8, 10, 12);
+    $m30 = array(4, 6, 9, 11);
+    ?>
     <div class="form">
-    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name"></input><br>
-        <label for="date">Date</label>
-        <select name="day" id="">
-            <?php
-            for ($i = 1; $i <= 31; $i++) {
-                print("<option>$i</option>");
-            }
-            ?>
-        </select>
-        <select name="month" id="">
-            <?php
-            for ($i = 1; $i <= 12; $i++) {
-                print("<option>$i</option>");
-            }
-            ?>
-        </select>
-        <select name="year" id="">
-            <?php
-            for ($i = 1980; $i <= 2030; $i++) {
-                print("<option>$i</option>");
-            }
-            ?>
-        </select><br>
-        <label for="time">Time</label>
-        <select name="second" id="">
-            <?php
-            for ($i = 1; $i <= 60; $i++) {
-                print("<option>$i</option>");
-            }
-            ?>
-        </select>
-        <select name="minute" id="">
-            <?php
-            for ($i = 1; $i <= 60; $i++) {
-                print("<option>$i</option>");
-            }
-            ?>
-        </select>
-        <select name="hour" id="">
-            <?php
-            for ($i = 1; $i <= 24; $i++) {
-                print("<option>$i</option>");
-            }
-            ?>
-        </select><br>
-        <button type="submit">Submit</button>
-        <button type="reset">Reset</button><br>
-    </form>
-    <?php 
-            $post = $_POST;
-            $name = $post['name'];
-            $second = $post['second'];
-            $minute = $post['minute'];
-            $hour = $post['hour'];
-            $day = $post['day'];
-            $month = $post['month'];
-            $year = $post['year'];
-            echo('Hi,'.$name.'!<br>');
-            echo('You have choose to have an appointment on '.$hour.':'.$minute.':'.$second.' '.$day.'/'.$month.'/'.$year);
-            echo('<br><br>More information:<br><br>');
-            echo('In 12 hours, the time and date is ');
-            if ($hour > 12){
-                echo($hour -12);
-                echo(':'.$minute.':'.$second.' PM<br>');
-            }
-            else{
-                echo($hour);
-                echo(':'.$minute.':'.$second.' AM<br>');
-            }
-            $m31 = array(1,3,5,7,8,10,12);
-            $m30 = array(4,6,9,11);
-            if (in_array($month, $m31)){
-                echo('This month has 31 days!');
-            }
-            elseif (in_array($month, $m30)){
-                echo('This month has 30 days!');
-            }
-            elseif ($month % 4 == 0 and $month % 100 != 0){
-                echo('This month has 28 days!');
-            }
-            else{
-                echo('This month has 29 days!');
-            }
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">    
+            <div class="name">
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" value="<?= isset($name)? $name:'' ?> " required></input><br>
+            </div>
+            <div class="date">
+                <label for="date">Date</label>
+                <input type="date" name="date" value="<?= isset($date)? $date:'' ?> " required><br>
+            </div>
+            <div class="time">
+                <label for="time">Time</label>
+                <select name="second" id="" required>
+                    <?php
+                    for ($i = 1; $i <= 60; $i++) {
+                        if ($second == $i)
+                            print("<option selected>$i</option>");
+                    }
+                    ?>
+                </select>
+                <label for="">s</label>
+                <select name="minute" id="" required>
+                    <?php
+                    for ($i = 1; $i <= 60; $i++) {
+                        if ($minute == $i)
+                            print("<option selected>$i</option>");
+                    }
+                    ?>
+                </select>
+                <label for="">m</label>
+                <select name="hour" id="" required>
+                    <?php
+                    for ($i = 1; $i <= 24; $i++) {
+                        if( $hour== $i)
+                            print("<option selected>$i</option>");
+                    }
+                    ?>
+                </select>
+                <label for="">h</label><br>
+            </div>
+            <div class="button">
+            <button type="submit">Submit</button>
+            <!-- <button type="reset" name="reset">Reset</button><br> -->
+            <input type="reset" value="Reset">
+                </div>
+        </form>
+    </div>
+    <div class="print">
+        <?php
+        echo ('Hi, <b>' . $name . '</b>!<br>');
+        echo ('You have choose to have an appointment on <b>' . $second . ':' . $minute . ':' . $hour . ' ' . $day . '/' . $month . '/' . $year . '</b>');
+        echo ('<br><br>More information:<br><br>');
+        echo ('In 12 hours, the time and date is ');
+        if ($hour > 12) {
+
+            echo ('<b>' . $second . ':' . $minute);
+            echo ('<b>:' . ($hour - 12) . '</b>PM</b><br>');
+        } else {
+
+            echo ('<b>' . $second . ':' . $minute);
+            echo ('<b>:' . $hour . '</b>AM</b><br>');
+        }
+        if (in_array($month, $m31)) {
+            echo ('This month has 31 days!');
+        } elseif (in_array($month, $m30)) {
+            echo ('This month has 30 days!');
+        } elseif ($month % 4 == 0 and $month % 100 != 0) {
+            echo ('This month has 28 days!');
+        } else {
+            echo ('This month has 29 days!');
+        }
 
         ?>
-        </div>
+    </div>
 </body>
+
 </html>
