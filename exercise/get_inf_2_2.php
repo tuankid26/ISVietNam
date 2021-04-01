7aadfe74ce2fe4a24b24b187fc33340651c67abc
Bùi Mạnh Tuấn <51566591+bmtuan@users.noreply.github.com>
2021-04-01 10:23:10 +0700

    exercise

--- exercise/get_inf_2_2.php
+++ exercise/get_inf_2_2.php
@@ -5,93 +5,115 @@
 </head>
 
 <body>
-<?php
+    <?php
     $post = $_POST;
-    $name = $post['name'];
-    $second = $post['second'];
-    $minute = $post['minute'];
-    $hour = $post['hour'];
-    $date = $post['date'];
-    $month = date('m', strtotime($date));
-    $year = date('y', strtotime($date));
-    $day = date('d', strtotime($date));
+    if (isset($post['name']))
+        $name = $post['name'];
+    if (isset($post['second']))
+        $second = $post['second'];
+    if (isset($post['minute']))
+        $minute = $post['minute'];
+    if (isset($post['hour']))
+        $hour = $post['hour'];
+    if (isset($post['date'])) {
+        $date = $post['date'];
+        $month = date('m', strtotime($date));
+        $year = date('y', strtotime($date));
+        $day = date('d', strtotime($date));
+    }
     $m31 = array(1, 3, 5, 7, 8, 10, 12);
     $m30 = array(4, 6, 9, 11);
     ?>
-    <div class="form">
-        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">    
+    <div class="form" id="form">
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
             <div class="name">
                 <label for="name">Name</label>
-                <input type="text" id="name" name="name" value="<?= isset($name)? $name:'' ?> " required></input><br>
+                <input type="text" id="name" name="name" value="<?= isset($name) ? $name : '' ?> " required></input><br>
             </div>
             <div class="date">
                 <label for="date">Date</label>
-                <input type="date" name="date" value="<?= isset($date)? $date:'' ?> " required><br>
+                <input type="date" id="date" name="date" value=<?= isset($date) ? $date : null ?> ><br>
             </div>
             <div class="time">
                 <label for="time">Time</label>
-                <select name="second" id="" required>
+                <select name="second" id="second" required>
                     <?php
-                    for ($i = 1; $i <= 60; $i++) {
+                    for ($i = 0; $i <= 59; $i++) {
                         if ($second == $i)
                             print("<option selected>$i</option>");
+                        else
+                            print("<option>$i</option>");
                     }
                     ?>
                 </select>
                 <label for="">s</label>
-                <select name="minute" id="" required>
+                <select name="minute" id="minute" required>
                     <?php
-                    for ($i = 1; $i <= 60; $i++) {
+                    for ($i = 0; $i <= 59; $i++) {
                         if ($minute == $i)
                             print("<option selected>$i</option>");
+                        else
+                            print("<option>$i</option>");
                     }
                     ?>
                 </select>
                 <label for="">m</label>
-                <select name="hour" id="" required>
+                <select name="hour" id="hour" required>
                     <?php
-                    for ($i = 1; $i <= 24; $i++) {
-                        if( $hour== $i)
+                    for ($i = 0; $i <= 23; $i++) {
+                        if ($hour == $i)
                             print("<option selected>$i</option>");
+                        else
+                            print("<option>$i</option>");
                     }
                     ?>
                 </select>
                 <label for="">h</label><br>
             </div>
             <div class="button">
-            <button type="submit">Submit</button>
-            <!-- <button type="reset" name="reset">Reset</button><br> -->
-            <input type="reset" value="Reset">
-                </div>
+                <button class="submit-button" type="submit" onclick="set_value()">Submit</button>
+                <button class="reset-button" onclick="reset_value()">Reset</button><br>
+            </div>
         </form>
     </div>
     <div class="print">
         <?php
-        echo ('Hi, <b>' . $name . '</b>!<br>');
-        echo ('You have choose to have an appointment on <b>' . $second . ':' . $minute . ':' . $hour . ' ' . $day . '/' . $month . '/' . $year . '</b>');
-        echo ('<br><br>More information:<br><br>');
-        echo ('In 12 hours, the time and date is ');
-        if ($hour > 12) {
+        if (isset($name)) {
+            echo ('Hi, <b>' . $name . '</b>!<br>');
+            echo ('You have choose to have an appointment on <b>' . $second . ':' . $minute . ':' . $hour . ' ' . $day . '/' . $month . '/' . $year . '</b>');
+            echo ('<br><br>More information:<br><br>');
+            echo ('In 12 hours, the time and date is ');
+            if ($hour > 12) {
 
-            echo ('<b>' . $second . ':' . $minute);
-            echo ('<b>:' . ($hour - 12) . '</b>PM</b><br>');
-        } else {
+                echo ('<b>' . $second . ':' . $minute);
+                echo ('<b>:' . ($hour - 12) . '</b> PM</b><br>');
+            } else {
 
-            echo ('<b>' . $second . ':' . $minute);
-            echo ('<b>:' . $hour . '</b>AM</b><br>');
-        }
-        if (in_array($month, $m31)) {
-            echo ('This month has 31 days!');
-        } elseif (in_array($month, $m30)) {
-            echo ('This month has 30 days!');
-        } elseif ($month % 4 == 0 and $month % 100 != 0) {
-            echo ('This month has 28 days!');
-        } else {
-            echo ('This month has 29 days!');
+                echo ('<b>' . $second . ':' . $minute);
+                echo ('<b>:' . $hour . '</b> AM</b><br>');
+            }
+            if (in_array($month, $m31)) {
+                echo ('This month has 31 days!');
+            } elseif (in_array($month, $m30)) {
+                echo ('This month has 30 days!');
+            } elseif ($month % 4 == 0 and $month % 100 != 0) {
+                echo ('This month has 28 days!');
+            } else {
+                echo ('This month has 29 days!');
+            }
         }
 
         ?>
     </div>
+    <script>
+        function reset_value(){
+            document.getElementById("name").value = "";
+            document.getElementById("date").value = null;
+            document.getElementById("second").value= 0;
+            document.getElementById("minute").value= 0;
+            document.getElementById("hour").value= 0;
+        }
+    </script>
 </body>
 
 </html>
