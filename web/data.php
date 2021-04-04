00ab6cb40b1710987c9e186136dac0c82a4dafd1
Bùi Mạnh Tuấn <51566591+bmtuan@users.noreply.github.com>
2021-04-04 22:36:37 +0700

    data

--- web/data.php
+++ web/data.php
@@ -11,16 +11,17 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-$sql = "SELECT id, name, sdt , email FROM test";
+
+
+$sql = "SELECT id_province, name FROM province";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-      echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["sdt"]. "<br>";
+      echo "id: " . $row["id_province"] . "<br>";
     }
   } else {
     echo "0 results";
   }
   $conn->close();
 ?>
-
